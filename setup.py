b5ec1c16eefa30281db6590feb49e5c846307828
jorgen <jorgen@karlsson.com>
2014-05-19 21:02:49 +0200

    license and classifiers

--- setup.py
+++ setup.py
@@ -11,9 +11,18 @@ setup(name='autoupgrade',
       version=__version__,
       author= 'Jörgen Karlsson',
       author_email='jorgen@karlsson.com',
-      license='MIT',
       description='Automatic upgrade of python modules and packages',
       long_description=open('README.txt').read(),
       packages=['autoupgrade'],
       url = "https://bitbucket.org/jorkar/autoupgrade",
-      install_requires = ["BeautifulSoup4"])
+      install_requires = ["BeautifulSoup4"],
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: System :: Software Distribution"
+        ]
+      )
